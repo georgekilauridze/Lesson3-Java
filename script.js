7a38c2e8675e914b4b6517d38d78b98da42cc360
georgekilauridze <99383455+georgekilauridze@users.noreply.github.com>
2022-04-21 14:14:00 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,101 @@
+
+/// Lesson 3 ex: 1
+
+// let array = [5, 25, 89, 120, 36];
+
+//    array.push('Javascript', 'python');
+//    array.unshift('html', 'css');
+//    array.shift();
+//    array.pop();
+
+//     console.log(array);
+
+/// ex: 2
+
+// let fruit = ['ფორთოხალი', 'ბანანი', 'მსხალი'];
+
+
+// fruit.push('ვაშლი', 'ანანასი');
+// console.log(fruit);
+
+// fruit.unshift('საზამთრო')
+// console.log(fruit);
+
+// fruit.splice(2, 0, 'მანგო');
+// console.log(fruit);
+
+// fruit.shift();
+// console.log(fruit);
+
+// fruit.pop();
+// console.log(fruit);
+
+/// ex: 3
+
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+
+// let result = array.map(item => item / 3)
+
+// console.log(result);
+
+/// ex: 4
+
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+
+// let result = array.filter(item => typeof item === 'number')
+
+// console.log(result);
+
+/// ex: 5
+
+// let languages = ['html', 'css', 'javascript', 'pyton', 'php'];
+
+// let result = languages.filter( item => item.length > 3);
+
+// console.log(result);
+
+
+/// ex: 6
+
+// let array = ['academy', ' of', ' digital', ' industries'].reduce(function(x, y) {
+//     return x+y;
+// },
+// '',
+// )
+// console.log(array);
+
+/// ex: 7
+
+// let item = [12, 'google', 32, null, 'yahoo', 25];
+
+// let result = item.map(item => {
+//     if (typeof item == 'number') {
+//         return item * item;
+//     } else if (typeof item == 'string') {
+//         return item.toLocaleUpperCase();
+//     }
+//     return item;
+// })
+// console.log(result);
+
+/// ex: 8
+
+let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+
+// let result = words.filter(function(item) {
+//     if (item.includes('m') || item.includes ('M')) {
+//         return true;
+//     }
+//         return false;
+// })
+
+// let result = words.filter(function(item) {
+//     if (item.toLocaleLowerCase().includes('m')) {
+//         return true;
+//     }
+//         return false;
+// })
+
+// console.log(result);
+
+
